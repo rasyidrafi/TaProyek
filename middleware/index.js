783db19a7c05b66f7902e3c4a443ca2312239023
rasyidrafi <rasyid.rafi@venturology.id>
2022-04-07 11:00:53 +0700

    test

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,13 @@
 let profile = localStorage.getItem("ta-proyek");
 if (profile) profile = JSON.parse(profile);
+let isLocal = false;
+let baseUrl = isLocal ? "" : "https://okok.my.id/ta-kel1/";
 
 let middleware = {
     mustLogin: () => {
-        if (!profile) return window.location.href = "./login.html";
+        if (!profile) return window.location.href = `${baseUrl}./login.html`;
     },
-    redirectIfLogin: (to = "./dashboard.html") => {
+    redirectIfLogin: (to = `${baseUrl}./dashboard.html`) => {
         if (profile) return window.location.href = to;
     }
 }
