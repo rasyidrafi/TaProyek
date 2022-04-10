2122b9f7271e66f6eb693dfcf293c6f589babc9d
Muhammad Hildan <57735377+hildannur@users.noreply.github.com>
2022-04-10 18:58:51 +0700

    Update register.php
    Memperbaiki fitur keamanan agar user tidak bisa menginputkan angka di dalam kolom username

--- register.php
+++ register.php
@@ -164,73 +164,186 @@ if (isset($_POST['register'])) {
             <?php endif; ?>
 
             <!-- form registrasi -->
-            <form action="" method="post" class="text-left">
-              <div class="form">
+              <form action="" method="post" class="text-left" >
+                <div class="form">
 
                 <!-- kolom email -->
-                <div id="email-field" class="field-wrapper input">
-                  <label for="email">EMAIL</label>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
-                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                  <input id="email" name="email" class="form-control" placeholder="Input Email" />
-                </div>
-
-                <!-- kolom username -->
-                <div id="username-field" class="field-wrapper input">
-                  <label for="username">USERNAME</label>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
-                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                  <input id="username" name="username" class="form-control" placeholder="Input username" />
-                </div>
-
-                <!-- kolom password -->
-                <div id="password-field" class="field-wrapper input mb-2">
-                  <div class="d-flex justify-content-between">
-                    <label for="password">PASSWORD</label>
+                  <div id="email-field" class="field-wrapper input">
+                    <label for="email">EMAIL</label>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-user"
+                    >
+                      <path
+                        d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
+                      ></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                    <input
+                      id="email"
+                      name="email"
+                      class="form-control"
+                      placeholder="Input Email"
+                    />
                   </div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock">
-                    <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-                    <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                  </svg>
-                  <input id="password" name="password" type="password" class="form-control" placeholder="Input Password" />
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye">
-                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                    <circle cx="12" cy="12" r="3"></circle>
-                  </svg>
-                </div>
 
-                <!-- kolom confirm password -->
-                <div id="password-field" class="field-wrapper input mb-2">
-                  <div class="d-flex justify-content-between">
-                    <label for="password">CONFIRM PASSWORD</label>
+                  <!-- kolom username -->
+                  <div id="username-field" class="field-wrapper input">
+                    <label for="username">USERNAME</label>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-user"
+                    >
+                      <path
+                        d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"
+                      ></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                    <input
+                      id="username"
+                      name="username"
+                      class="form-control"
+                      placeholder="Input username"
+                      onkeypress="return event.charCode < 48 || event.charCode  >57"
+                    />
                   </div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock">
-                    <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-                    <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                  </svg>
-                  <input id="password" name="repassword" type="password" class="form-control" placeholder="Input Password" />
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye">
-                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                    <circle cx="12" cy="12" r="3"></circle>
-                  </svg>
-                </div>
 
-                <div class="d-sm-flex justify-content-between">
-                  <div class="field-wrapper">
-                    <button class="btn btn-primary" type="submit" name="register" value="">Register</button>
+                  <!-- kolom password -->
+                  <div id="password-field" class="field-wrapper input mb-2">
+                    <div class="d-flex justify-content-between">
+                      <label for="password">PASSWORD</label>
+                    </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-lock"
+                    >
+                      <rect
+                        x="3"
+                        y="11"
+                        width="18"
+                        height="11"
+                        rx="2"
+                        ry="2"
+                      ></rect>
+                      <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                    </svg>
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      class="form-control"
+                      placeholder="Input Password"
+                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      id="toggle-password"
+                      class="feather feather-eye"
+                    >
+                      <path
+                        d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
+                      ></path>
+                      <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
                   </div>
-                </div>
 
-                <p class="signup-link">
-                  Was registered ?
-                  <a href="login.php">Log in to your account</a>
-                </p>
-              </div>
-            </form>
+                  <!-- kolom confirm password -->
+                  <div id="password-field" class="field-wrapper input mb-2">
+                    <div class="d-flex justify-content-between">
+                      <label for="password">CONFIRM PASSWORD</label>
+                    </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-lock"
+                    >
+                      <rect
+                        x="3"
+                        y="11"
+                        width="18"
+                        height="11"
+                        rx="2"
+                        ry="2"
+                      ></rect>
+                      <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                    </svg>
+                    <input
+                      id="password"
+                      name="repassword"
+                      type="password"
+                      class="form-control"
+                      placeholder="Input Confirm Password"
+                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      id="toggle-password"
+                      class="feather feather-eye"
+                    >
+                      <path
+                        d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"
+                      ></path>
+                      <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                  </div>
+
+                  <div class="d-sm-flex justify-content-between">
+                    <div class="field-wrapper">
+                      <button class="btn btn-primary" type="submit" name="register" value="">Register</button>
+                    </div>
+                  </div>
+
+                  <p class="signup-link">
+                    Was registered ?
+                    <a href="Login.php">Log in to your account</a>
+                  </p>
+                </div>
+              </form>
 
           </div>
         </div>
