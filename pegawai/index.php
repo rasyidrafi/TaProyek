b715f0175a43fb158baf1aca3d479af19a0ac19e
Mei-Juli <103299083+Mei-Juli@users.noreply.github.com>
2022-04-13 13:03:26 +0700

    Data Pegawai

--- pegawai/index.php
+++ pegawai/index.php
@@ -15,9 +15,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Pegawai</title>
 </head>
 <body>
-    <h4>Selamat datang di halaman index pegawai</h4>
+    <h4>Selamat datang di halaman data pegawai</h4>
 </body>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      Data Pegawai
+    </a>
+  </div>
+</nav>
+ <!-- judul -->
+ <div class="container">
+        <h1 class="mt-3">Data Pegawai</h1>
+    <figure>
+        <blockquote class="blockquote">
+            <p>Berisi data yang telah tersimpan di database</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+            CRUD <cite title="Source Title">Create Read Update Delete</cite>
+        </figcaption>
+    </figure>
+        <button type="button" class="btn btn-primary">Tambah Data</button>
+        <div class="table-responsive">
+    <table class="table align-middle">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Username</th> 
+                <th>Email</th>
+                <th>Alamat</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                <button type="button" class="btn btn-danger btn-sm">Hapus</button>
+                <button type="button" class="btn btn-success btn-sm">Edit</button>
+                </td>
+
+
+            </tr>
+        </tbody>
+    </table>    
+</div>
+</div>
 </html>
