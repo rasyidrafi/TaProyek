38505b3dd5233ba15b4fcb9382adf9eb7f81bd50
firza401 <muhammadfirzaalifikri@gmail.com>
2022-05-14 14:13:55 +0700

    Sidebar

--- component/_sidebar.php
+++ component/_sidebar.php
@@ -8,7 +8,7 @@
             </div>
         </div>
         <div class="shadow-bottom"></div>
-
+    <!-- BEGIN SIDEBAR ADMIN -->
         <?php if ($_SESSION["role"] == "admin") {
         ?>
             <ul class="list-unstyled menu-categories" id="accordionExample">
@@ -74,6 +74,30 @@
         <?php
 
         } ?>
+    <!-- END SIDEBAR ADMIN -->
+
+    <!-- BEGIN SIDEBAR PEGAWAI -->
+        <?php if ($_SESSION["role"] == "pagawai-1") {
+        ?>
+
+            <ul class="list-unstyled menu-categories" id="accordionExample">
+                <li class="menu">
+                    <a href="data-bahan.php" aria-expanded="false" class="dropdown-toggle">
+                        <div class="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay">
+                                <path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path>
+                                <polygon points="12 15 17 21 7 21 12 15"></polygon>
+                            </svg>
+                            <span> Pegawai</span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        <?php
+
+        } ?>
+    <!-- END SIDEBAR PEGAWAI -->
+
     </nav>
 </div>
 <!--  END SIDEBAR  -->
