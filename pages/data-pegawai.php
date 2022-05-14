2f38f544dc9f8916354736036dddf7f6220e6e17
rasyidrafi <rasyid.rafi@yahoo.com>
2022-05-14 14:15:27 +0700

    data pegawai 1/2

--- pages/data-pegawai.php
+++ pages/data-pegawai.php
@@ -51,11 +51,22 @@ if ($result && mysqli_num_rows($result)) {
                                             <td><?= $value['username'] ?></td>
                                             <td><?= $value['email'] ?></td>
                                             <td class="text-capitalize"><?= $value['role'] ?></td>
-                                            <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle table-cancel">
+                                            <td>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle table-cancel">
                                                     <circle cx="12" cy="12" r="10"></circle>
                                                     <line x1="15" y1="9" x2="9" y2="15"></line>
                                                     <line x1="9" y1="9" x2="15" y2="15"></line>
-                                                </svg></td>
+                                                </svg>
+
+                                                <?php if ($_SESSION['role'] == 'admin') {
+                                                ?>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-unlock">
+                                                        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                                                        <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
+                                                    </svg>
+                                                <?php
+                                                } ?>
+                                            </td>
                                         </tr>
 
                                     <?php
