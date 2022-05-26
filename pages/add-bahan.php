eb2e16355c685d9fac4933e77a6f248e065d3631
Rasyid Rafi <69502214+rasyidrafi@users.noreply.github.com>
2022-05-26 21:41:14 +0700

    fix bahan

--- pages/add-bahan.php
+++ pages/add-bahan.php
@@ -16,7 +16,7 @@ if (isset($_POST['nama']) && isset($_POST['stok_awal']) && isset($_POST['satuan'
         $porsi = $_POST['porsi'];
         $satuan_porsi = $_POST['satuan_porsi'];
 
-        $query = "INSERT INTO bahan (nama, stok_awal, satuan, porsi, satuan_porsi) VALUES ('$nama', '$stok_awal', '$satuan')";
+        $query = "INSERT INTO bahan (nama, stok_awal, satuan, porsi, satuan_porsi) VALUES ('$nama', '$stok_awal', '$satuan', '$porsi', '$satuan_porsi')";
         $result = mysqli_query($conn, $query);
 
         if ($result) {
